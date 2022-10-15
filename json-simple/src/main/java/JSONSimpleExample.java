af91118ba063074b55018f30ba1c0a7ee12857c7
vitzhakun <vizarce@gmail.com>
2022-10-16 00:26:44 +0300

    adding first changes

--- json-simple/src/main/java/JSONSimpleExample.java
+++ json-simple/src/main/java/JSONSimpleExample.java
@@ -5,12 +5,16 @@ import org.json.simple.parser.ParseException;
 public class JSONSimpleExample {
 
 	public static void main(String[] args) throws ParseException {
-		String json = "{\"name\": \"John\", \"age\": 32, \"job\": \"developer\"}";
+		String json = "{\"name\": \"Samuel\", \"age\": 35, \"job\": \"developer\"}";
 		JSONParser parser = new JSONParser();
 		Object obj = parser.parse(json);
 		JSONObject jsonObject = (JSONObject) obj;
 		String name = (String) jsonObject.get("name");
+		Long age = (Long) jsonObject.get("age");
+		String job = (String) jsonObject.get("job");
 		System.out.println(name);
+		System.out.println(age);
+		System.out.println(job);
 	
 
 	}
